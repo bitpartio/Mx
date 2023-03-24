87200496b9e60f36eb71d088adc607a4cbc33a5b
Michael Chisari <michael@chisari.us>
2023-03-23 23:43:32 -0500

    Update test

--- examples/html-test-page/main_test.go
+++ examples/html-test-page/main_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"testing"
 
-	. "github.com/bitpartio/gomx/elements"
 	. "github.com/bitpartio/gomx/utils"
 )
 
@@ -14,42 +13,7 @@ func BenchmarkRender(b *testing.B) {
 }
 
 func render() {
-	button := Button(
-		ButtonProps{
-			GlobalProps: GlobalProps{
-				Autocapitalize: GlobalOptions.Autocapitalize.Words,
-				Dir:            GlobalOptions.Dir.Auto,
-				Data:           DataValues{"current": "", "second": "four"},
-				Aria:           AriaRoles{"current": "page"},
-				Htmx:           HtmxProps{"post": "/clicked"},
-			},
-			InnerHTML: "hello, world",
-			Disabled:  true,
-		},
-	)
+	page := buildHtmlTestPage()
 
-	hr := Hr(HrProps{})
-	hello := B(BProps{InnerHTML: "Bold Hello"})
-	header := Header(
-		HeaderProps{
-			GlobalProps: GlobalProps{
-				Data: DataValues{"current": "this is the current page"},
-			},
-			InnerHTML: button,
-		},
-	)
-
-	children := Stack(
-		header,
-		hr,
-		hello,
-	)
-
-	output := HTML(
-		HTMLProps{
-			InnerHTML: children,
-			Lang:      "en",
-		},
-	)
-	_ = Minify(output)
+	_ = Minify(page)
 }
