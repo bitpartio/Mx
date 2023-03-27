9910d36b252fc22ef7022ab4434b4e9d59f6f2b5
Michael Chisari <michael@chisari.us>
2023-03-26 20:20:22 -0500

    Added static benchmark for comparison

--- examples/html-test-page/main_test.go
+++ examples/html-test-page/main_test.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	. "github.com/bitpartio/Mx/utils"
 )
+
+var staticPage string
+
+func init() {
+	staticPageBytes, err := os.ReadFile("html-test-page.html")
+	if err != nil {
+		fmt.Println("Could not read html test page:", err)
+	} else {
+		staticPage = string(staticPageBytes)
+	}
+}
 
 func BenchmarkRender(b *testing.B) {
 	for n := 0; n < b.N; n++ {
@@ -16,4 +29,14 @@ func render() {
 	page := buildHtmlTestPage()
 
 	_ = Minify(page)
+}
+
+func BenchmarkRenderStatic(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		renderStatic()
+	}
+}
+
+func renderStatic() {
+	_ = Minify(staticPage)
 }
