0b87ba6f7097ad7b1d370ac55c728bbfa7014b8c
Michael Chisari <michael@chisari.us>
2023-04-05 21:52:42 -0500

    Added body functions

--- elements/sectioning_root.go
+++ elements/sectioning_root.go
@@ -11,12 +11,50 @@ import . "github.com/bitpartio/Mx/utils"
 type BodyProps struct {
 	GlobalProps
 
+	Onafterprint     string
+	Onbeforeprint    string
+	Onbeforeunload   string
+	Onblur           string
+	Onerror          string
+	Onfocus          string
+	Onhashchange     string
+	Onlanguagechange string
+	Onload           string
+	Onmessage        string
+	Onoffline        string
+	Ononline         string
+	Onpopstate       string
+	Onredo           string
+	Onresize         string
+	Onstorage        string
+	Onundo           string
+	Onunload         string
+
 	InnerHTML string
 }
 
 func Body(props BodyProps) string {
 	values := map[string]interface{}{
 		"global": BuildGlobalProps(props.GlobalProps),
+
+		"onafterprint":     BuildProp("onafterprint", props.Onafterprint),
+		"onbeforeprint":    BuildProp("onbeforeprint", props.Onbeforeprint),
+		"onbeforeunload":   BuildProp("onbeforeunload", props.Onbeforeunload),
+		"onblur":           BuildProp("onblur", props.Onblur),
+		"onerror":          BuildProp("onerror", props.Onerror),
+		"onfocus":          BuildProp("onfocus", props.Onfocus),
+		"onhashchange":     BuildProp("onhashchange", props.Onhashchange),
+		"onlanguagechange": BuildProp("onlanguagechange", props.Onlanguagechange),
+		"onload":           BuildProp("onload", props.Onload),
+		"onmessage":        BuildProp("onmessage", props.Onmessage),
+		"onoffline":        BuildProp("onoffline", props.Onoffline),
+		"ononline":         BuildProp("ononline", props.Ononline),
+		"onpopstate":       BuildProp("onpopstate", props.Onpopstate),
+		"onredo":           BuildProp("onredo", props.Onredo),
+		"onresize":         BuildProp("onresize", props.Onresize),
+		"onstorage":        BuildProp("onstorage", props.Onstorage),
+		"onundo":           BuildProp("onundo", props.Onundo),
+		"onunload":         BuildProp("onunload", props.Onunload),
 
 		"innerhtml": props.InnerHTML,
 	}
